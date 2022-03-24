777e5484d8a80da741c527b470928d777e4538a5
EmptyTuple <kotorov@outlook.com>
2022-03-24 22:28:11 +0700

    YaDisk uploader modified

--- YaDisk-uploader.py
+++ YaDisk-uploader.py
@@ -1,14 +1,12 @@
 import requests
 import os
 
-def get_token(path: str) -> str:
-    '''Функция считывает токен из текстового файла по указанному пути'''
+def _get_token() -> str:
+    '''Функция считывает токен из текстового файла по указанному пути. 
+       Проверок на правильность ввода не делал'''
     path_to_token = input('Enter path to file with token: ')
-    try:
-        with open(path, 'r') as f:
+    with open(path_to_token, 'r') as f:
             token = f.readline()
-    except FileNotFoundError:
-        print("File with token is not found")
     return token
 
 class YaUploader:
@@ -16,6 +14,7 @@ class YaUploader:
         self.token = token
 
     def upload(self, file_path: str):
+        '''Загрузчик. Проверок на правильность ввода не делал.'''
         response = requests.get(url='https://cloud-api.yandex.net/v1/disk/resources/upload',
                 headers={'Authorization': 'OAuth ' + self.token},
                 params={'path': os.path.basename(file_path), 'overwrite': 'true'},)
@@ -29,7 +28,7 @@ class YaUploader:
             print("Success!")
 
 if __name__ == '__main__':
-    TOKEN_PATH = os.path.join(os.getcwd(), 'token.txt')
     path_to_file = input('Enter path to file you want upload: ')
-    ya = YaUploader(get_token(TOKEN_PATH))
+    ya = YaUploader(_get_token())
     ya.upload(path_to_file)
+    
